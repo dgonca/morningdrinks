539826b1652f26a98b2cc085510a2eb5e7cc8f8b
dgonca <domstar1595@yahoo.com>
2018-08-01 12:56:35 -0500

    landing page up and server running

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/index', to: 'static_pages#index'
+  root 'static_pages#index'
+
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
