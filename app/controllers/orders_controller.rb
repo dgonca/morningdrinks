0ab3c9ed050d040bf68b288d371708a3c9e3ac89
dgonca <domstar1595@yahoo.com>
2018-08-02 14:30:39 -0500

    working on new order form processing correctly

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,8 +10,23 @@ class OrdersController < ApplicationController
   end
 
   def create
+    #creating the order as well as the drink
+    p params[:order][:drink_attributes]
+    @order = Order.new(user_id: current_user.id, order_status: "in_progress")
+    @drink = Drink.new(params[:order][:drink_attributes])
+    @drink.order_id = @order.id
+    p @order
+    p @drink
   end
 
   def destroy
   end
+
+  private
+
+  # def drink_params
+  #     params.require(:order[:drink_attributes]).permit(:drink_type, :coffee_type, :sugar_level, :milk_level)
+  # end
+
+
 end
